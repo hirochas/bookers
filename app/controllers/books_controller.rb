98d7aa7ca1117532ebf9d8f651425ec94f98fb2b
HiromiYoshizaki <115017300+hirochas@users.noreply.github.com>
2022-10-29 08:57:40 +0000

    toukoupage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+     book = Book.new(book_params)
+     book.save
+     redirect_to '/'
   end
 
   def index
-    @books = Books.all
+     @books = Book.all
+
   end
 
   def show
@@ -11,4 +19,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
- 
-  get "/" => "homes#top"
-  
-  resources :books
+
+  root to: "homes#top"
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  resources :'book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
